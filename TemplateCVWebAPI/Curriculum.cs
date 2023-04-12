31b0df90a2c9dc0502c518a50ae140ae51e13d84
albertpm95 <albertpm95@outlook.com>
2023-04-12 16:21:05 +0200

    Agregar archivos de proyecto.

---
+++ TemplateCVWebAPI/Curriculum.cs
@@ -0,0 +1,9 @@
+﻿namespace TemplateCVWebAPI
+{
+    public class Curriculum
+    {
+        public string Name { get; set; } = "Albert";
+        public string Apellidos { get; set; } = "Pastor Martínez";
+        public string FechaNacimiento { get; set; } = DateOnly(8 / 02 / 1995);
+    }
+}
